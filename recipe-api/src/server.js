27153e1173233409caf6da5829a691ecbc535122
Tuliops <Tuliopirir3@gmail.com>
2025-10-09 00:58:34 -0600

    000

--- recipe-api/src/server.js
+++ recipe-api/src/server.js
@@ -56,10 +56,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://recipelboxfotosgt.z13.web.core.windows.net',
-     'http://localhost:5173',
-    'https://recipelboxfotosgt.z13.web.core.windows.net',
-    'http://recipeboxfotosgt.blob.core.windows.net'
+    'https://recipelboxfotosgt.z13.web.core.windows.net'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
